3254c958caf40d55aca8c7f793b09e23adb40ea8
Boris Sarafimov <boris.101743@student.ugd.edu.mk>
2018-06-09 20:52:21 +0200

    better var names + bug fix

--- script-grafana.sh
+++ script-grafana.sh
@@ -1,38 +1,71 @@
 #!/bin/bash
 
-echo "Checking the environment.."
-apt-get -y install -qq pcregrep
-OUTPUT="$(ls /etc/nginx/sites-enabled)"
-OLO="$(pcregrep -M  'server.*\n.*listen 9100' /etc/nginx/sites-enabled/$OUTPUT)"; len=${#OLO};
-if [[ "$len" -gt "5" ]]
+read -p "Select 'n' if you are using NGINX or 'a' if you are using APACHE2 (n/a)?" server
+
+if [[ "$server" = "a" ]]
 then
-        echo 'Please modify the sites-enabled/* in nginx'; exit
-else
-        echo 'We are good to go..'
+    a2enmod proxy
+    a2enmod proxy_http
+    a2enmod proxy_balancer
+    a2enmod lbmethod_byrequests
+    selected_site="$(ls /etc/apache2/sites-enabled)"
+    read -p "Do you have supervisord installed and configured? (y/n)?" supervisor
+    if [[ "supervisor" = "y" ]]
+    then
+        ufw allow 9001
+        echo "
+Listen 9001
+<VirtualHost *:9001>
+        ServerName supervisord
+
+        CustomLog /var/log/apache2/supervisord_access.log combined
+        ErrorLog /var/log/apache2/supervisord_error.log
+
+        ProxyRequests off
+        <Proxy *>
+                AuthType        Basic
+                AuthName        \"Enter Password\"
+                AuthUserFile    /var/www/.htpasswd
+                Require valid-user
+                Allow from all
+        </Proxy>
+
+        ProxyErrorOverride On
+        ProxyPass / http://127.0.0.1:19001/
+        ProxyPassReverse / http://127.0.0.1:19001/
+</VirtualHost>
+" >> /etc/apache2/sites-enabled/$selected_site;
+
+        echo "
+
+[inet_http_server]
+port = *:19001
+" >> /etc/supervisor/supervisord.conf;
+        systemctl daemon-reload
+        systemctl restart supervisor.service
+    fi
+
+    systemctl restart apache2.service
+# If you have NGINX environment
+elif [[ "$server" = "n" ]]
+then
+        selected_site="$(ls /etc/nginx/sites-enabled)"
 fi
 
-echo "Script started..";
-useradd --no-create-home --shell /bin/false node_exporter
-read -p "Would you like to install supervisord (y/n)?" choice
-case "$choice" in 
-  y|Y ) echo "yes";;
-  n|N ) echo "no";;
-  * ) echo "invalid";;
-esac
-
-echo "Downloading Node Exporter";
-
-cd ~
-curl -LO https://github.com/prometheus/node_exporter/releases/download/v0.15.2/node_exporter-0.15.2.linux-amd64.tar.gz
-
-echo "Installing Node Exporter";
-
-tar xvf node_exporter-0.15.2.linux-amd64.tar.gz
-cp node_exporter-0.15.2.linux-amd64/node_exporter /usr/local/bin
-chown node_exporter:node_exporter /usr/local/bin/node_exporter
-rm -rf node_exporter-0.15.2.linux-amd64.tar.gz node_exporter-0.15.2.linux-amd64
-touch /etc/systemd/system/node_exporter.service
-echo "
+read -p "Do you want to install NodeExporter-v0.15.2 (y/n)?" node
+if [[ "$node" = "y" ]]
+then
+        ufw allow 9100
+        useradd --no-create-home --shell /bin/false node_exporter
+        echo "Downloading NodeExporter-v.0.15.2";
+        cd ~
+        curl -LO https://github.com/prometheus/node_exporter/releases/download/v0.15.2/node_exporter-0.15.2.linux-amd64.tar.gz
+        tar xvf node_exporter-0.15.2.linux-amd64.tar.gz
+        cp node_exporter-0.15.2.linux-amd64/node_exporter /usr/local/bin
+        chown node_exporter:node_exporter /usr/local/bin/node_exporter
+        rm -rf node_exporter-0.15.2.linux-amd64.tar.gz node_exporter-0.15.2.linux-amd64
+        touch /etc/systemd/system/node_exporter.service
+        echo "
 [Unit]
 Description=Node Exporter
 Wants=network-online.target
@@ -41,74 +74,40 @@ After=network-online.target
 [Service]
 User=node_exporter
 Group=node_exporter
-ExecStart=/usr/local/bin/node_exporter --web.listen-address=:19100 --collectors.supervisor.url=collector.supervisord.url=http://127.0.0.1:9001/
+ExecStart=/usr/local/bin/node_exporter --web.listen-address=:19100 --collector.supervisord.url="http://tom:u49t4GqVpNnz@127.0.0.1:19001/RPC"
+
 
 [Install]
 WantedBy=multi-user.target
 " > /etc/systemd/system/node_exporter.service;
 
-if [[ "$choice" -eq "y" ]]
-then
-    echo "Installing supervisord";
-    apt-get -y install python-setuptools
-    easy_install supervisor
-    mkdir /etc/supervisor
-    echo_supervisord_conf > /etc/supervisor/supervisord.conf
-    echo "
-[include]
-files = conf.d/*.conf
+        if [[ "$server" = "a" ]]
+        then
+                echo "
+Listen 9100
+<VirtualHost *:9100>
+        ServerName NodeExporter
 
-[inet_http_server]
-port=*:9001
-    " >> /etc/supervisor/supervisord.conf;
-    mkdir /etc/supervisor/conf.d
-    touch /etc/systemd/system/supervisord.service
-    echo "
-[Unit]
-Description=Supervisor daemon
-Documentation=http://supervisord.org
-After=network.target
+        CustomLog /var/log/apache2/nodeexporter_access.log combined
+        ErrorLog /var/log/apache2/nodeexporter_error.log
 
-[Service]
-ExecStart=/usr/local/bin/supervisord -n -c /etc/supervisor/supervisord.conf
-ExecStop=/usr/local/bin/supervisorctl $OPTIONS shutdown
-ExecReload=/usr/local/bin/supervisorctl $OPTIONS reload
-KillMode=process
-Restart=on-failure
-RestartSec=42s
+        ProxyRequests off
+        <Proxy *>
+                AuthType        Basic
+                AuthName        \"Enter Password\"
+                AuthUserFile    /var/www/.htpasswd
+                Require valid-user
+                Allow from all
+        </Proxy>
 
-[Install]
-WantedBy=multi-user.target
-Alias=supervisord.service
-    " >> /etc/systemd/system/supervisord.service;
-    systemctl daemon-reload
-    systemctl start supervisord.service
-fi
-
-read -p "Would you like to add credentials to the node_exporter so random people can't access it? (y/n)?" credent
-case "$credent" in 
-  y|Y ) echo "yes";;
-  n|N ) echo "no";;
-  * ) echo "invalid";;
-esac
-
-if [[ "$credent" -eq "y" ]]
-then
-    echo "Installing apache2-utils";
-    apt-get -y install apache2-utils
-    echo "Please input the username and than the password:";
-    read username;
-    htpasswd -c /etc/nginx/.htpasswd $username
-else
-    touch /etc/nginx/.htpasswd
-fi
-
-echo "Final steps..";
-apt-get -y install iptables-persistent
-ufw allow 9100
-ufw allow 9001
-netfilter-persistent save
-echo "
+        ProxyErrorOverride On
+        ProxyPass / http://127.0.0.1:19100/
+        ProxyPassReverse / http://127.0.0.1:19100/
+</VirtualHost>
+" >> /etc/apache2/sites-enabled/$selected_site;
+        elif [[ "$server" = "n" ]]
+        then
+                echo "
 server {
     listen 9100 default_server;
     listen [::]:9100 default_server;
@@ -116,7 +115,7 @@ server {
     location / {
         auth_basic '"Prometheus server authentication"';
         auth_basic_user_file /etc/nginx/.htpasswd;
-        proxy_pass http://localhost:19100;
+        proxy_pass http://127.0.0.1:19100;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection 'upgrade';
@@ -125,8 +124,11 @@ server {
     }
 
 }
-" >> /etc/nginx/sites-enabled/*;
+" >> /etc/nginx/sites-enabled/$selected_site;
 systemctl daemon-reload
-systemctl start node_exporter
-systemctl enable node_exporter
+systemctl start node_exporter.service
+systemctl enable node_exporter.service
 nginx -s reload
+        fi
+fi
+echo "Script completed";
